91c5523de312f69c4279ef708e824927b4961458
Per Christian <pc@kvalvik.no>
2019-09-27 20:46:53 +0200

    add python server

---
+++ server.py
@@ -0,0 +1,27 @@
+import json
+import pyvjoy
+import socket
+
+
+def main():
+    joystick = pyvjoy.VJoyDevice(1)
+    
+    host = '0.0.0.0'
+    port = 14854
+    
+    # Create a UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((host, port))
+    
+    # Continuously read data from the socket
+    print(f'Server setup, connect to {socket.gethostbyname(socket.gethostname())}:{port}')
+    while True:
+        data, addr = sock.recvfrom(1024)
+        left, right = json.loads(data.decode())
+        
+        joystick.set_axis(pyvjoy.HID_USAGE_X, left)
+        joystick.set_axis(pyvjoy.HID_USAGE_Y, right)
+
+
+if __name__ == '__main__':
+    main()
